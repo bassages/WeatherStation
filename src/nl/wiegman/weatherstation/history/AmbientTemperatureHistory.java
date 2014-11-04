48af66e1faf138673123d3fbe855c741a09c54b1
Rob Wiegman <bassages@gmail.com>
2014-11-04 10:50:32 +0100

    Clean up

--- src/nl/wiegman/weatherstation/history/AmbientTemperatureHistory.java
+++ src/nl/wiegman/weatherstation/history/AmbientTemperatureHistory.java
@@ -1,43 +1,22 @@
 package nl.wiegman.weatherstation.history;
 
-import java.util.List;
-
 import nl.wiegman.weatherstation.sensorvaluelistener.AmbientTemperatureListener;
 import android.content.Context;
-import android.os.AsyncTask;
 
 /**
  * Maintains the ambient temperature history
  */
-public class AmbientTemperatureHistory implements AmbientTemperatureListener {
+public class AmbientTemperatureHistory extends AbstractSensorHistory implements AmbientTemperatureListener {
 
-	private static final String DB_SENSOR_NAME = "ambient_temperature";
+	private static final String SENSOR_NAME = "ambient_temperature";
 	
 	@Override
 	public void ambientTemperatureUpdate(final Context context, final Double updatedTemperature) {
-		// Do not block the UI thread, by using an aSyncTask
-		AsyncTask<Void,Void,Void> asyncTask = new AsyncTask<Void, Void, Void>() {
-			@Override
-			protected Void doInBackground(Void... params) {
-				SensorValueHistoryDatabase.getInstance(context).addSensorValue(DB_SENSOR_NAME, updatedTemperature);
-				return null;
-			}
-		};
-		asyncTask.execute();
+		registerUpdatedValue(context, updatedTemperature);
 	}
 
-	public void deleteAll(final Context context) {
-		AsyncTask<Void,Void,Void> asyncTask = new AsyncTask<Void, Void, Void>() {
-			@Override
-			protected Void doInBackground(Void... params) {
-				SensorValueHistoryDatabase.getInstance(context).deleteAll(DB_SENSOR_NAME);
-				return null;
-			}
-		};
-		asyncTask.execute();
-	}
-	
-	public List<SensorValueHistoryItem> getAll(Context context) {
-		return SensorValueHistoryDatabase.getInstance(context).getAllHistory(DB_SENSOR_NAME);
+	@Override
+	protected String getSensorName() {
+		return SENSOR_NAME;
 	}
 }
--- src/nl/wiegman/weatherstation/history/ObjectTemperatureHistory.java
+++ src/nl/wiegman/weatherstation/history/ObjectTemperatureHistory.java
@@ -1,43 +1,22 @@
 package nl.wiegman.weatherstation.history;
 
-import java.util.List;
-
 import nl.wiegman.weatherstation.sensorvaluelistener.ObjectTemperatureListener;
 import android.content.Context;
-import android.os.AsyncTask;
 
 /**
  * Maintains the object temperature history
  */
-public class ObjectTemperatureHistory implements ObjectTemperatureListener {
+public class ObjectTemperatureHistory extends AbstractSensorHistory implements ObjectTemperatureListener {
 
-	private static final String DB_SENSOR_NAME = "object_temperature";
+	private static final String SENSOR_NAME = "object_temperature";
 	
 	@Override
 	public void objectTemperatureUpdate(final Context context, final Double updatedTemperature) {
-		// Do not block the UI thread, by using an aSyncTask
-		AsyncTask<Void,Void,Void> asyncTask = new AsyncTask<Void, Void, Void>() {
-			@Override
-			protected Void doInBackground(Void... params) {
-				SensorValueHistoryDatabase.getInstance(context).addSensorValue(DB_SENSOR_NAME, updatedTemperature);
-				return null;
-			}
-		};
-		asyncTask.execute();
+		registerUpdatedValue(context, updatedTemperature);
 	}
 
-	public void deleteAll(final Context context) {
-		AsyncTask<Void,Void,Void> asyncTask = new AsyncTask<Void, Void, Void>() {
-			@Override
-			protected Void doInBackground(Void... params) {
-				SensorValueHistoryDatabase.getInstance(context).deleteAll(DB_SENSOR_NAME);
-				return null;
-			}
-		};
-		asyncTask.execute();
-	}
-	
-	public List<SensorValueHistoryItem> getAll(Context context) {
-		return SensorValueHistoryDatabase.getInstance(context).getAllHistory(DB_SENSOR_NAME);
+	@Override
+	protected String getSensorName() {
+		return SENSOR_NAME;
 	}
 }
